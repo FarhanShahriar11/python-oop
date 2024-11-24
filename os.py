e6c698e4856caedfa11812ebd976214bfabf95db
FarhanShahriar11 <farhanshahriar014@gmail.com>
2024-11-24 12:20:24 +0600

    first commit

--- os.py
+++ os.py
@@ -3,6 +3,10 @@ cd=os.getcwd()
 print(cd)
 # /////////////
 
+import os #importing os module
+size = os.path.getsize("f.txt")
+print("Size of the file is", size," bytes.")
+
 f = open("f.txt", "r")
 print(f.read())
 
