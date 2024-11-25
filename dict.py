6017b1c1e432f91a3fdda089e0f97f60ec06667a
FarhanShahriar11 <farhanshahriar014@gmail.com>
2024-11-25 13:04:45 +0600

    first commit

---
+++ dict.py
@@ -0,0 +1,21 @@
+
+# Dictionaries are used to store data values in key:value pairs.
+
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates
+
+
+
+thisdict={"brand": "Ford",
+  "model": "Mustang",
+  "year": 1964}
+print(thisdict["model"])
+
+#///////////////////////////////////
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
