a0c5afb0e5a59b8f850f8c3c472514342e231781
FarhanShahriar11 <farhanshahriar014@gmail.com>
2024-11-25 11:48:48 +0600

    first commit

---
+++ packing_unpacking.py
@@ -0,0 +1,13 @@
+
+#We use two operators * (for tuples) and ** (for dictionaries).
+
+
+# def func(a,b,c,d):
+#     print(a,b,c,d)
+# mylist=[1,2,3,4]
+# func(mylist)
+#.............................
+def func(a,b,c,d):
+    print(a,b,c,d)
+mylist=[1,2,3,4]
+func(*mylist)
